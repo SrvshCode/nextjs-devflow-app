6989ffd8fd914eb558889c360b40d3cb92de0a0d
SrvshCode <vmardolker@gmail.com>
2025-03-18 08:49:21 +0400

    tailwind css setup with legacy-peer-deps

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
-import React from 'react'
+import React from "react";
 
-const Home = () =>  (
-    <h1>Welcome to Exciting world of Next js</h1>
-  )
+const Home = () => (
+  <h1 className="font-black text-3xl text-primary-500">
+    Welcome to Exciting world of Next js
+  </h1>
+);
 
-
-export default Home
+export default Home;
