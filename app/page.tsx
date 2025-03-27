063b5487fe0827702a8f5e1ba7007b20fb70123d
SrvshCode <vmardolker@gmail.com>
2025-03-27 21:32:04 +0400

    Setting up fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import React from "react";
 
 const Home = () => (
-  <h1 className="font-black text-3xl text-primary-500">
-    Welcome to Exciting world of Next js
-  </h1>
+  <>
+    <h1 className="h1-bold font-inter">
+      Welcome to Exciting world of Next js[inter]
+    </h1>
+    <h1 className="h1-bold font-space-grotesk">
+      Welcome to Exciting world of Next js[space grotesk]
+    </h1>
+  </>
 );
 
 export default Home;
